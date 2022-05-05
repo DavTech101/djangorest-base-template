a841dbb2426641a896f802ad18f8442fc3609816
DavTech <dav-tech@outlook.com>
2022-05-05 21:57:27 +0200

    Product and Customer models created

--- api/store/models.py
+++ api/store/models.py
@@ -1,3 +1,27 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Customer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
