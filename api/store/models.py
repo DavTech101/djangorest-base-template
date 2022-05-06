483ca9186544c6afa649043ae3074f086b760dfb
DavTech <dav-tech@outlook.com>
2022-05-06 22:12:06 +0200

    removes created at for products and migrates

--- api/store/models.py
+++ api/store/models.py
@@ -28,7 +28,6 @@ class Product(models.Model):
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion, blank=True)
 
