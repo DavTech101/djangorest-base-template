aeaeb75402142bdc8377775e99437db3f90703b8
DavTech <dav-tech@outlook.com>
2022-05-06 13:36:45 +0200

    Migrations setup

--- api/store/models.py
+++ api/store/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
