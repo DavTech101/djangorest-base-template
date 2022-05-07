28c3957e403503ec24a108787b66405da3418f89
DavTech <dav-tech@outlook.com>
2022-05-07 16:36:10 +0200

    adds string representation method to models

--- api/store/models.py
+++ api/store/models.py
@@ -9,7 +9,7 @@ class Promotion(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
 
 
@@ -18,6 +18,9 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         "Product", on_delete=models.SET_NULL, null=True, blank=True, related_name="+"
     )  # Does not create reverse relationship
+
+    def __str__(self) -> str:
+        return self.title
 
 
 class Product(models.Model):
@@ -31,7 +34,7 @@ class Product(models.Model):
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion, blank=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
 
 
@@ -56,7 +59,7 @@ class Customer(models.Model):
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
     )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
     class Meta:
@@ -72,6 +75,9 @@ class Address(models.Model):
         Customer, on_delete=models.CASCADE, primary_key=True
     )
     zipcode = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.street}, {self.city}, {self.zipcode}"
 
 
 class Order(models.Model):
@@ -92,12 +98,18 @@ class Order(models.Model):
     )
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    def __str__(self) -> str:
+        return f"{self.payment_status} - {self.customer.first_name}"
+
 
 class OrderItem(models.Model):  # OrderItem is a join table
     quantity = models.PositiveSmallIntegerField()
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.product} - {self.quantity}"
 
 
 class Cart(models.Model):
@@ -109,3 +121,6 @@ class CartItem(models.Model):  # CartItem is a join table
     quantity = models.PositiveSmallIntegerField()
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.product} - {self.quantity}"
