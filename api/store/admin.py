fc97a0724db9e51936505421ca1a970879733b5b
DavTech <dav-tech@outlook.com>
2022-05-07 17:33:44 +0200

    Collections admin configured

--- api/store/admin.py
+++ api/store/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
-from .models import Collection, Customer, Product, Promotion
+from django.http import HttpRequest
+from django.db.models.aggregates import Count
+from .models import Collection, Customer, Product, Order
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_per_page = 100
-    list_editable = ("inventory", "unit_price")
-    list_display = ("title", "inventory", "unit_price")
+    list_editable = ("unit_price",)
+    list_display = ("title", "inventory_status", "collection", "unit_price")
+
+    @admin.display(ordering="inventory")
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return "Low"
+
+        return "OK"
 
 
 @admin.register(Customer)
@@ -16,5 +25,21 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "membership")
 
 
-admin.site.register(Promotion)
-admin.site.register(Collection)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_per_page = 100
+    list_select_related = ("customer",)
+    list_display = ("id", "customer", "placed_at")
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_per_page = 100
+    list_display = ("title", "featured_product", "products_count")
+
+    @admin.display(ordering="products_count") 
+    def products_count(self, collection):
+        return collection.products_count
+
+    def get_queryset(self, request: HttpRequest):
+        return super().get_queryset(request).annotate(products_count=Count("product"))
