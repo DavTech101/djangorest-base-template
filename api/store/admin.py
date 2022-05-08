03367dec8c8a456434bb42a9e9974da59d3714ba
DavTech <dav-tech@outlook.com>
2022-05-08 22:24:18 +0200

    Shows product counts for orders and order counts for customers

--- api/store/admin.py
+++ api/store/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.urls import reverse
 from django.http import HttpRequest
+from django.utils.html import format_html
 from django.db.models.aggregates import Count
 from .models import Collection, Customer, Product, Order
 
@@ -22,7 +24,15 @@ class ProductAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_per_page = 100
     list_editable = ("membership",)
-    list_display = ("first_name", "last_name", "membership")
+    list_display = ("first_name", "last_name", "membership", "orders_count")
+
+    @admin.display(ordering="orders_count")
+    def orders_count(self, customer):
+        url = f"{reverse('admin:store_order_changelist')}?customer={customer.id}"
+        return format_html(f"<a href={url}>{customer.orders_count}</a>")
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(orders_count=Count("order"))
 
 
 @admin.register(Order)
@@ -37,9 +47,10 @@ class CollectionAdmin(admin.ModelAdmin):
     list_per_page = 100
     list_display = ("title", "featured_product", "products_count")
 
-    @admin.display(ordering="products_count") 
+    @admin.display(ordering="products_count")
     def products_count(self, collection):
-        return collection.products_count
+        url = f"{reverse('admin:store_product_changelist')}?collection__id={collection.id}"
+        return format_html(f"<a href='{url}'>{collection.products_count}</a>")
 
     def get_queryset(self, request: HttpRequest):
         return super().get_queryset(request).annotate(products_count=Count("product"))
