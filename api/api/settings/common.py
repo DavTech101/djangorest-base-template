746845fbc53543dfd1ee2b705f1a203b8147d5e1
DavTech <dav-tech@outlook.com>
2022-05-12 15:47:43 +0200

    updated packages and requirement files and added Djangrestframework to installed_apps

--- api/api/settings/common.py
+++ api/api/settings/common.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "debug_toolbar",
     "store",
     "store_custom",
