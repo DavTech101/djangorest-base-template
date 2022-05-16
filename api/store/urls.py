962150f86cce214cf460a19f7879005d64479d72
DavTech <dav-tech@outlook.com>
2022-05-16 13:44:48 +0200

    configures filtering for products on collection_id

--- api/store/urls.py
+++ api/store/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 
 router = DefaultRouter()
-router.register("products", views.ProductViewSet)
+router.register("products", views.ProductViewSet, basename="products")
 router.register("collections", views.CollectionViewSet)
 
 product_router = NestedDefaultRouter(router, "products", lookup="product")
