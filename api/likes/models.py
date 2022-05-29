609889944d8881c09e65192c3f6d1f9a047a13d9
DavTech <dav-tech@outlook.com>
2022-05-29 13:39:59 +0200

    Adds config for custom User model in Core app

--- api/likes/models.py
+++ api/likes/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
@@ -10,4 +9,3 @@ class LikedItem(models.Model):
     object_id = models.PositiveIntegerField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     content_object = GenericForeignKey("content_type", "object_id")
- 
